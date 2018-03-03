a020195113baeafc1d35ae7dfbce3ff58dd244b8
bloc-notes <philippedoyon@outlook.com>
2018-03-03 16:35:05 -0500

    tabulation plz

--- LodeRunnerScripts.js
+++ LodeRunnerScripts.js
@@ -8,35 +8,35 @@ var objC2D = null;
 var intTailleCases = 30 ;
 var tabObjMurs = null;
 var tableau =[
-        [0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,4],
-        [0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,4],
-        [0,0,1,0,0,0,0,3,3,3,1,0,0,0,0,1,4],
-        [0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,4],
-        [0,0,1,0,0,0,0,1,0,0,1,0,0,3,3,1,4],
-        [0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,1,4],
-        [0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,1,4],
-        [0,0,3,3,3,3,3,1,0,0,1,0,0,1,0,1,4],
-        [0,0,1,2,0,0,0,0,0,0,1,0,0,1,0,1,4],
-        [0,0,1,2,0,0,0,0,0,0,3,3,3,1,0,1,4],
-        [0,0,1,2,0,0,0,0,0,0,1,0,2,1,0,1,4],
-        [0,0,1,2,0,0,0,0,0,0,1,0,2,0,0,1,4],
-        [0,0,1,2,1,1,1,1,0,0,1,0,2,0,0,1,4],
-        [0,0,1,2,1,1,1,1,0,0,1,0,2,0,0,1,4],
-        [0,0,1,2,3,3,3,1,0,0,1,0,2,0,0,1,4],
-        [0,0,0,2,0,0,0,1,0,0,1,0,2,0,0,1,4],
-        [0,0,0,2,0,0,0,1,0,0,1,0,2,0,0,1,4],
-        [0,0,0,2,0,0,0,1,0,0,1,0,2,0,0,1,4],
-        [0,0,0,0,1,0,0,1,0,0,1,0,2,0,0,1,4],
-        [0,0,0,0,1,0,0,1,0,0,1,0,2,0,0,1,4],
-        [0,0,0,0,1,0,0,3,3,3,3,3,3,1,0,1,4],
-        [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
-        [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
-        [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
-        [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
-        [0,0,0,0,3,3,3,1,0,0,0,0,0,1,0,1,4],
-        [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
-        [0,0,0,0,1,0,0,1,0,0,0,0,0,3,3,1,4]
-     ];
+    [0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,4],
+    [0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,4],
+    [0,0,1,0,0,0,0,3,3,3,1,0,0,0,0,1,4],
+    [0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,4],
+    [0,0,1,0,0,0,0,1,0,0,1,0,0,3,3,1,4],
+    [0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,1,4],
+    [0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,1,4],
+    [0,0,3,3,3,3,3,1,0,0,1,0,0,1,0,1,4],
+    [0,0,1,2,0,0,0,0,0,0,1,0,0,1,0,1,4],
+    [0,0,1,2,0,0,0,0,0,0,3,3,3,1,0,1,4],
+    [0,0,1,2,0,0,0,0,0,0,1,0,2,1,0,1,4],
+    [0,0,1,2,0,0,0,0,0,0,1,0,2,0,0,1,4],
+    [0,0,1,2,1,1,1,1,0,0,1,0,2,0,0,1,4],
+    [0,0,1,2,1,1,1,1,0,0,1,0,2,0,0,1,4],
+    [0,0,1,2,3,3,3,1,0,0,1,0,2,0,0,1,4],
+    [0,0,0,2,0,0,0,1,0,0,1,0,2,0,0,1,4],
+    [0,0,0,2,0,0,0,1,0,0,1,0,2,0,0,1,4],
+    [0,0,0,2,0,0,0,1,0,0,1,0,2,0,0,1,4],
+    [0,0,0,0,1,0,0,1,0,0,1,0,2,0,0,1,4],
+    [0,0,0,0,1,0,0,1,0,0,1,0,2,0,0,1,4],
+    [0,0,0,0,1,0,0,3,3,3,3,3,3,1,0,1,4],
+    [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
+    [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
+    [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
+    [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
+    [0,0,0,0,3,3,3,1,0,0,0,0,0,1,0,1,4],
+    [0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,4],
+    [0,0,0,0,1,0,0,1,0,0,0,0,0,3,3,1,4]
+    ];
 
 function initAnimation(Canvas){
     objCanvas = Canvas;
@@ -45,6 +45,56 @@ function initAnimation(Canvas){
     initMurs();
     initPointage();
     dessiner();
+}
+
+function initMurs() {
+    tabObjMurs = new Array();
+    var objMur = null;
+
+    // Le mur de gauche (#0)
+    objMur = new Object();
+    objMur.intXDebut = 0;
+    objMur.intYDebut = 0;
+    objMur.intXFin = 30;
+    objMur.intYFin = objCanvas.height
+    objMur.strCouleur = 'darkgrey';
+    tabObjMurs.push(objMur);
+
+    // Le mur du centre (en haut) (#1)
+    objMur = new Object();
+    objMur.intXDebut = 0;
+    objMur.intYDebut = 0;
+    objMur.intXFin = objCanvas.width;
+    objMur.intYFin = 30;
+    objMur.strCouleur = 'darkgrey';
+    tabObjMurs.push(objMur);
+
+    // Le mur de droite (#2)
+    objMur = new Object();
+    objMur.intXDebut = objCanvas.width;
+    objMur.intYDebut = 0;
+    objMur.intXFin = objCanvas.width - 30;
+    objMur.intYFin = objCanvas.height;
+    objMur.strCouleur = 'darkgrey';
+    tabObjMurs.push(objMur);
+    //mur bas
+    objMur = new Object();
+    objMur.intXDebut = 0;
+    objMur.intYDebut = objCanvas.height;
+    objMur.intXFin = objCanvas.width;
+    objMur.intYFin = objCanvas.height -30;
+    objMur.strCouleur = 'darkgrey';
+    tabObjMurs.push(objMur);
+}
+
+function initPointage(){
+    objPointage = new Object();
+    objPointage.score = 0;
+    objPointage.scoreNiveauPrec = 0 ;
+    objPointage.tempsNiveau = 0;
+    objPointage.tempsTotal = 0;
+    objPointage.vies = 5;
+    objPointage.niveau =1;
 }
 
 function dessiner() {
@@ -83,87 +133,40 @@ function dessinerTableau(){
         }
     }
 }
-    function initMurs() {
-        tabObjMurs = new Array();
-        var objMur = null;
+    
+function dessinerMurs(){
+    objC2D.save();
+    objC2D.globalAlpha = 0.8;
 
-        // Le mur de gauche (#0)
-        objMur = new Object();
-        objMur.intXDebut = 0;
-        objMur.intYDebut = 0;
-        objMur.intXFin = 30;
-        objMur.intYFin = objCanvas.height
-        objMur.strCouleur = 'darkgrey';
-        tabObjMurs.push(objMur);
-
-        // Le mur du centre (en haut) (#1)
-        objMur = new Object();
-        objMur.intXDebut = 0;
-        objMur.intYDebut = 0;
-        objMur.intXFin = objCanvas.width;
-        objMur.intYFin = 30;
-        objMur.strCouleur = 'darkgrey';
-        tabObjMurs.push(objMur);
-
-        // Le mur de droite (#2)
-        objMur = new Object();
-        objMur.intXDebut = objCanvas.width;
-        objMur.intYDebut = 0;
-        objMur.intXFin = objCanvas.width - 30;
-        objMur.intYFin = objCanvas.height;
-        objMur.strCouleur = 'darkgrey';
-        tabObjMurs.push(objMur);
-        //mur bas
-        objMur = new Object();
-        objMur.intXDebut = 0;
-        objMur.intYDebut = objCanvas.height;
-        objMur.intXFin = objCanvas.width;
-        objMur.intYFin = objCanvas.height -30;
-        objMur.strCouleur = 'darkgrey';
-        tabObjMurs.push(objMur);
+    for (var intNoMur = 0; intNoMur < tabObjMurs.length; intNoMur++) {
+        var objMur = tabObjMurs[intNoMur];
+        var intCentreX = (objMur.intXDebut + objMur.intXFin) / 2;
+        var intCentreY = (objMur.intYDebut + objMur.intYFin) / 2;
+        var intRayon = ((intNoMur == 1)||(intNoMur ==3)) ? Math.abs(objMur.intXFin - objMur.intXDebut) / 2 : Math.abs(objMur.intYFin - objMur.intYDebut) / 2;
+        var objDegrade = objC2D.createRadialGradient(intCentreX, intCentreY, 0, intCentreX, intCentreY, intRayon);
+        objDegrade.addColorStop(0, 'white');
+        objDegrade.addColorStop(1, objMur.strCouleur);
+        objC2D.fillStyle = objDegrade;
+        objC2D.beginPath();
+        objC2D.moveTo(objMur.intXDebut, objMur.intYDebut);
+        objC2D.lineTo(objMur.intXFin, objMur.intYDebut);
+        objC2D.lineTo(objMur.intXFin, objMur.intYFin);
+        objC2D.lineTo(objMur.intXDebut, objMur.intYFin);
+        objC2D.closePath();
+        objC2D.fill();
     }
+    objC2D.restore();
+}
 
-    function dessinerMurs(){
-        objC2D.save();
-	    objC2D.globalAlpha = 0.8;
+function dessinerPointage(){
+    objC2D.save();
+    var strTextePointage= "Vies : "+objPointage.vies + "  Niveau : " + objPointage.niveau + " Pointage : " + objPointage.score +" Temps : "+objPointage.tempsNiveau;
+    
+    objC2D.fillStyle = 'white';
+    objC2D.font = '20px Arial';
+    objC2D.textBaseLine = 'middle';
+    objC2D.textAlign = 'left';
+    objC2D.fillText(strTextePointage,30,18*intTailleCases);
+    objC2D.restore();
 
-	    for (var intNoMur = 0; intNoMur < tabObjMurs.length; intNoMur++) {
-	        var objMur = tabObjMurs[intNoMur];
-	        var intCentreX = (objMur.intXDebut + objMur.intXFin) / 2;
-	        var intCentreY = (objMur.intYDebut + objMur.intYFin) / 2;
-	        var intRayon = ((intNoMur == 1)||(intNoMur ==3)) ? Math.abs(objMur.intXFin - objMur.intXDebut) / 2 : Math.abs(objMur.intYFin - objMur.intYDebut) / 2;
-	        var objDegrade = objC2D.createRadialGradient(intCentreX, intCentreY, 0, intCentreX, intCentreY, intRayon);
-	        objDegrade.addColorStop(0, 'white');
-	        objDegrade.addColorStop(1, objMur.strCouleur);
-	        objC2D.fillStyle = objDegrade;
-	        objC2D.beginPath();
-	        objC2D.moveTo(objMur.intXDebut, objMur.intYDebut);
-	        objC2D.lineTo(objMur.intXFin, objMur.intYDebut);
-	        objC2D.lineTo(objMur.intXFin, objMur.intYFin);
-	        objC2D.lineTo(objMur.intXDebut, objMur.intYFin);
-	        objC2D.closePath();
-	        objC2D.fill();
-	    }
-	    objC2D.restore();
-    }
-    function initPointage(){
-        objPointage = new Object();
-        objPointage.score = 0;
-        objPointage.scoreNiveauPrec = 0 ;
-        objPointage.tempsNiveau = 0;
-        objPointage.tempsTotal = 0;
-        objPointage.vies = 5;
-        objPointage.niveau =1;
-    }
-    function dessinerPointage(){
-        objC2D.save();
-        var strTextePointage= "Vies : "+objPointage.vies + "  Niveau : " + objPointage.niveau + " Pointage : " + objPointage.score +" Temps : "+objPointage.tempsNiveau;
-        
-        objC2D.fillStyle = 'white';
-        objC2D.font = '20px Arial';
-        objC2D.textBaseLine = 'middle';
-        objC2D.textAlign = 'left';
-        objC2D.fillText(strTextePointage,30,18*intTailleCases);
-        objC2D.restore();
-
-    }
+}
